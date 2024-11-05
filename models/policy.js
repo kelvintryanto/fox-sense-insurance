cdc2cf3d1361b2d7f2c76e7d1c65c95879cdc98c
kelvintryanto <kelvin.tryanto@gmail.com>
2024-11-05 23:28:07 +0700

    feat: creating db and model

---
+++ models/policy.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Policy extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Policy.init({
+    profileId: DataTypes.INTEGER,
+    policyName: DataTypes.STRING,
+    policyType: DataTypes.STRING,
+    premium: DataTypes.INTEGER,
+    policyCreated: DataTypes.STRING,
+    agentId: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Policy',
+  });
+  return Policy;
+};
