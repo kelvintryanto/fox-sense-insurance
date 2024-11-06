453a576dec8ec6a8e594569a61531413870bbb95
kelvintryanto <kelvin.tryanto@gmail.com>
2024-11-07 02:13:03 +0700

    feat: membuat static method untuk agentController, policyController, dan profileController

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -1,3 +1,52 @@
-class AgentController {}
+// tidak perlu mengerjakan kalau tidak ada waktu, karena ini untuk super admin
+class AgentController {
+  static async readAgent(req, res) {
+    try {
+      
+    } catch (error) {
+      res.send(error)
+    }
+  }
+  
+  static async addAgentForm(req, res) {
+    try {
+      
+    } catch (error) {
+      res.send(error)
+    }
+  }
+  
+  static async createAgent(req, res) {
+    try {
+      
+    } catch (error) {
+      res.send(error)
+    }
+  }
+  
+  static async updateAgentForm(req, res) {
+    try {
+      
+    } catch (error) {
+      res.send(error)
+    }
+  }
+  
+  static async updateAgent(req, res) {
+    try {
+      
+    } catch (error) {
+      res.send(error)
+    }
+  }
+  
+  static async deleteAgent(req, res) {
+    try {
+      
+    } catch (error) {
+      res.send(error)
+    }
+  }
+}
 
 module.exports = AgentController;
--- controllers/policyController.js
+++ controllers/policyController.js
@@ -1,3 +1,45 @@
-class PolicyController {}
+class PolicyController {
+  static async readPolicy(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async addPolicyForm(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async createPolicy(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async updatePolicyForm(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async updatePolicy(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async deletePolicy(req, res) {
+    try {
+    } catch (error) {
+      res.send(error);
+    }
+  }
+}
 
 module.exports = PolicyController;
